a6e5c39eda5e24f750f2cf31991f22aaa1041449
Dan Tian <dantian@Dans-MacBook-Pro.local>
2016-03-30 11:28:02 -0400

    enabled bootloader backdoor in ccfg

--- startup_files/ccfg.c
+++ startup_files/ccfg.c
@@ -205,17 +205,17 @@
 #endif
 
 #ifndef SET_CCFG_BL_CONFIG_BL_LEVEL
-// #define SET_CCFG_BL_CONFIG_BL_LEVEL                  0x0        // Active low to open boot loader backdoor
-#define SET_CCFG_BL_CONFIG_BL_LEVEL                     0x1        // Active high to open boot loader backdoor
+#define SET_CCFG_BL_CONFIG_BL_LEVEL                     0x0        // Active low to open boot loader backdoor
+//#define SET_CCFG_BL_CONFIG_BL_LEVEL                   0x1        // Active high to open boot loader backdoor
 #endif
 
 #ifndef SET_CCFG_BL_CONFIG_BL_PIN_NUMBER
-#define SET_CCFG_BL_CONFIG_BL_PIN_NUMBER                0xFF       // DIO number for boot loader backdoor
+#define SET_CCFG_BL_CONFIG_BL_PIN_NUMBER                0x1C       // DIO number for boot loader backdoor
 #endif
 
 #ifndef SET_CCFG_BL_CONFIG_BL_ENABLE
-// #define SET_CCFG_BL_CONFIG_BL_ENABLE                 0xC5       // Enabled boot loader backdoor
-#define SET_CCFG_BL_CONFIG_BL_ENABLE                    0xFF       // Disabled boot loader backdoor
+#define SET_CCFG_BL_CONFIG_BL_ENABLE                    0xC5       // Enabled boot loader backdoor
+//#define SET_CCFG_BL_CONFIG_BL_ENABLE                  0xFF       // Disabled boot loader backdoor
 #endif
 
 //#####################################
